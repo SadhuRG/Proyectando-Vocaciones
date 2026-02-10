e66c4575466f5f148c171ad0984e668c81d91f79
SadhuRG <Andresrojas1234321@gmail.com>
2026-02-10 16:26:07 -0500

    second commit

--- src/pages/carrerras.jsx
+++ src/pages/carrerras.jsx
@@ -2,8 +2,8 @@
 export default function Carreras() {
   return (
     <div>
-      <h1>Página de Carreras</h1>
-      <p>Aquí irán las áreas y carreras...</p>
+      <h1 className="text-3xl font-bold text-center mb-4">Página de Carreras</h1>
+      <p className="text-center text-green-500">Aquí irán las áreas y carreras...</p>
     </div>
   );
 }
